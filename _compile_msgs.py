b61f71b93b7d254d2ffd8787d35c9b0afb8c8416
Leratos <48392090+leratos@users.noreply.github.com>
2026-02-20 05:59:28 +0100

    chore: move compile_msgs.py to dev-scripts/

--- _compile_msgs.py
+++
@@ -1,5 +0,0 @@
-import polib
-
-po = polib.pofile(r"C:\Dev\Berry-Gym\locale\en\LC_MESSAGES\django.po")
-po.save_as_mofile(r"C:\Dev\Berry-Gym\locale\en\LC_MESSAGES\django.mo")
-print(f"Compiled {len(po.translated_entries())} translations to django.mo")
