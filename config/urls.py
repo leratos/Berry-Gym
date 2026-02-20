c1469272df340eea18b357a279d36fa26501b1df
Leratos <48392090+leratos@users.noreply.github.com>
2026-02-20 15:29:19 +0100

    fix: isort import order in test_i18n.py and urls.py

--- config/urls.py
+++ config/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 """
 
 from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
     path("i18n/", include("django.conf.urls.i18n")),  # set_language endpoint
